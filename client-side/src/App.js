3754e94a03bd80d942eaa7e1ae47c68431095adf
goldmeninalmaty <146926829+Ulyk04@users.noreply.github.com>
2025-05-02 22:54:23 +0300

    Update App.js

--- client-side/src/App.js
+++ client-side/src/App.js
@@ -2,13 +2,23 @@ import {Routes , Route} from 'react-router-dom'
 import Home from './pages/Home'
 import WelcomePage from './pages/WelcomePage';
 import LoginPage from './pages/LoginPage';
+import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<WelcomePage/>} />
-      <Route path='/home' element={<Home/>} />
       <Route path='/login' element={<LoginPage/>} />
+
+      <Route 
+          path='/home' 
+          element={
+                <ProtectedRoute>
+                  <Home/>
+                </ProtectedRoute>
+                } 
+      />
+      
     </Routes>
   );
 }
