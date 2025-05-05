6c577146905b6ce6cb17686298a03054b363fd01
goldmeninalmaty <146926829+Ulyk04@users.noreply.github.com>
2025-05-05 22:03:39 +0300

    Update ProtectedRoute.jsx

--- client-side/src/components/ProtectedRoute.jsx
+++ client-side/src/components/ProtectedRoute.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { Navigate } from 'react-router-dom';
 
 export default function ProtectedRoute({ children }) {
-  const isAuthenticated = localStorage.getItem('isAuthenticated') === 'true';
+  const isAuthenticated = localStorage.getItem('isAuth') === 'true';
 
   return isAuthenticated ? children : <Navigate to="/login" replace />;
 }
